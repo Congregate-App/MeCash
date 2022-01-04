63278e17e82eeb07af865c7856450dbd0388bebb
ImmArkorful <e.arkorful3@gmail.com>
2022-01-04 19:37:19 +0000

    progress

--- src/components/AppButton.js
+++ src/components/AppButton.js
@@ -1,14 +1,13 @@
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import React from 'react';
+import {StyleSheet, Text, TouchableOpacity, Dimensions} from 'react-native';
 
-import colors from "../config/colors";
-
-function AppButton({ title, onPress, color = "green", style}) {
+import colors from '../config/colors';
+const PAGE_HEIGHT = Dimensions.get('window').height;
+function AppButton({title, onPress, color = 'green', style}) {
   return (
     <TouchableOpacity
-      style={[styles.button, style,{ backgroundColor: colors[color] }]}
-      onPress={onPress}
-    >
+      style={[styles.button, style, {backgroundColor: colors[color]}]}
+      onPress={onPress}>
       <Text style={styles.text}>{title}</Text>
     </TouchableOpacity>
   );
@@ -18,17 +17,18 @@ const styles = StyleSheet.create({
   button: {
     backgroundColor: colors.primary,
     borderRadius: 15,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 15,
-    width: "40%",
+    width: '40%',
+    height: PAGE_HEIGHT / 15,
     marginVertical: 10,
   },
   text: {
     color: colors.white,
     fontSize: 18,
-    textTransform: "uppercase",
-    fontWeight: "bold",
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
   },
 });
 
